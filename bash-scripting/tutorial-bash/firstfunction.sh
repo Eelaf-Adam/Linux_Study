6334f35e8d424fa07631018e1dc66a0c1c07aef1
Eelaf-Adam <e.adam@alustudent.com>
2025-06-01 17:55:44 +0200

    11th freecodecamp-functions

--- bash-scripting/tutorial-bash/firstfunction.sh
+++ bash-scripting/tutorial-bash/firstfunction.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-
+up="beforce"
+since="function"
+echo $up
+echo $since
 showuptime(){
-	up=$(uptime -p | cut -c4-)
-	since=$(uptime -s)
+	local up= $(uptime -p | cut -c4-)
+	local since= $(uptime -s)
 	cat << EOF
 -----
 This machine has been up for ${up}
@@ -11,4 +14,6 @@ It has been running since ${since}
 EOF
 }
 showuptime
+echo $up
+echo $since
 
