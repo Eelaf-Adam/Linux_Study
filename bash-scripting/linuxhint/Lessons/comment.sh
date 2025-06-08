34fd1a666d9f3218ffd13bc1e9c3e163a6d8b84d
Eelaf-Adam <e.adam@alustudent.com>
2025-06-08 19:30:14 +0200

    Bash comments

---
+++ bash-scripting/linuxhint/Lessons/comment.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# 1. This is a single or a one line comment 
+
+# 2. This is a multiline comment
+
+: '
+this is a cat command
+this is a cat command 
+this is a cat command
+this is a cat command
+this is a cat command
+this is a cat command
+this is a cat command
+this is a cat command
+this is a cat command
+this is a cat command'
+
+
+
+
